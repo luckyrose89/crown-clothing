93b6f1cc4bfd8423d7591d36be5c3163d20587e3
avidrutham <divyamathur89@gmail.com>
2020-06-08 21:37:01 +0530

    implemented firebase utils, included storing data to firestore feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homePage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -19,8 +19,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => console.log(this.state)
+          );
+        });
+      }
+      this.setState({
+        currentUser: userAuth,
+      });
     });
   }
 
@@ -29,7 +46,6 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.currentUser);
     return (
       <div>
         <Header currentUser={this.state.currentUser} />
