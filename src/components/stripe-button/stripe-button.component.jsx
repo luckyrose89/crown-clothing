8ad89ed45ad9496458da614b9ef2c59859ecc4e6
avidrutham <divyamathur89@gmail.com>
2020-06-25 22:18:59 +0530

    integrate react stripe checkout and build stripeCheckoutButton component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51GxxdHKf1CtSxFdrowYToiXQhB31w24YxHrt58q4P8Zk3kwMeWcs3XnrJ3EFY3KITWJgWbzg7xGSvdo20LzkGajO00SndCMQoq";
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="crown clothing"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
