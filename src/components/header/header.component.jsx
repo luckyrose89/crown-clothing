629ed35df00258cd37e7ed72e2eb5d0f6e313a5f
avidrutham <divyamathur89@gmail.com>
2020-07-16 20:01:25 +0530

    create with-spinner higher order component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
